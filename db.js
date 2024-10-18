97a454fc311738f62b590663c663c69586e32589
gitbubiltu <biltupal20190@gmail.com>
2024-10-18 21:56:15 +0530

    shit mongo to online server

--- db.js
+++ db.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/hotels';
-
+require('dotenv').config()
+// const mongoURL = 'mongodb://localhost:27017/hotels';
 // Connect to MongoDB without deprecated options
+// const mongoURL=process.env.MONGODB_URL_LOCAL;
+const mongoURL=process.env.MONGODB_URL;
 mongoose.connect(mongoURL);
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const db = require('./db'); // Ensure correct path to the db file/module
-
+require('dotenv').config();
 const bodyParser=require('body-parser');
 app.use(bodyParser.json()); //req.body
 
-
+const PORT=process.env.PORT || 3000;
 // Respond with "Welcome to my hotel" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('Welcome to my hotel...How can I help you? We have a list of menus.');
@@ -17,6 +17,7 @@ const menuItemRoutes=require('./routes/menuItemRoutes')
 app.use('/person',personRoutes);
 app.use('/menu',menuItemRoutes)
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log('Server listening on port 3000');
 });
