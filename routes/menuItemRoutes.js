d0feebba449d77ec1e1c986b8b6307c22de7a44c
gitbubiltu <biltupal20190@gmail.com>
2024-10-18 22:33:26 +0530

    Small changes

--- routes/menuItemRoutes.js
+++ routes/menuItemRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const MenuItem =require('./../models/MenuItem')
+const MenuItem =require('../models/MenuItem')
 
 //post route to add a menu
 router.post('/',async(req,res)=>{
